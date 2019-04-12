1369d4c519bf80215c6169d672af4e79e0dc5e82
Chen Roth <rothchen@gmail.com>
2019-04-12 13:02:25 +0300

    add Block component with typed props

---
+++ src/Block.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+export interface IBlockProps {
+    color: 'red' | 'green' | 'blue';
+    text: string;
+}
+
+export class Block extends React.Component<IBlockProps> {
+    render() {
+        const { color, text } = this.props;
+        const style = { backgroundColor: color, width: '50px', height: '50px' };
+        return (
+            <div style={style}>
+                {text}
+            </div>
+        );
+
+    }
+}
